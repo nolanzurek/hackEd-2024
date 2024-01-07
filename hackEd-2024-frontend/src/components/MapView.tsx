7aa91577d27093996ebaed13d44ac5e69a4ba7f2
Nolan Zurek <nolanzurek@gmail.com>
2024-01-07 09:42:43 -0700

    Add markers for various tables in MapView component

--- hackEd-2024-frontend/src/components/MapView.tsx
+++ hackEd-2024-frontend/src/components/MapView.tsx
@@ -7,10 +7,45 @@ import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import { fetchTableData, TableData } from "../lib/supabase";
 
-
 const MapView = () => {
+  const tables: String[] = [
+    "BikeRacksAndRepairStands",
+    "Neighbourhoods",
+    "Parks",
+    "PropertyAssessments",
+    "RainfallGauge",
+    "TrafficDisruptions",
+    "VegetationAreasNaturalized",
+    "busStopsByLandmark",
+    "trees",
+  ];
+
+  //   tables
+  const [parkMarkers, setParkMarkers] = useState<TableData<"Parks">[]>([]);
+  const [bikeRackMarkers, setBikeRackMarkers] = useState<
+    TableData<"BikeRacksAndRepairStands">[]
+  >([]);
+  const [neighbourhoodMarkers, setNeighbourhoodMarkers] = useState<
+    TableData<"Neighbourhoods">[]
+  >([]);
+  const [propertyAssessmentMarkers, setPropertyAssessmentMarkers] = useState<
+    TableData<"PropertyAssessments">[]
+  >([]);
+  const [rainfallGaugeMarkers, setRainfallGaugeMarkers] = useState<
+    TableData<"RainfallGauge">[]
+  >([]);
+  const [trafficDisruptionMarkers, setTrafficDisruptionMarkers] = useState<
+    TableData<"TrafficDisruptions">[]
+  >([]);
+  const [vegetationAreaMarkers, setVegetationAreaMarkers] = useState<
+    TableData<"VegetationAreasNaturalized">[]
+  >([]);
+  const [busStopMarkers, setBusStopMarkers] = useState<
+    TableData<"busStopsByLandmark">[]
+  >([]);
+  const [treeMarkers, setTreeMarkers] = useState<TableData<"trees">[]>([]);
+
   const [selectedOption, setSelectedOption] = useState("Any Neighbourhood");
-  const [parkMarkers, setParkMarkers] = useState<TableData<'Parks'>[]>([]);
 
   const handleOptionChange = (event: any) => {
     setSelectedOption(event.target.value);
@@ -19,7 +54,15 @@ const MapView = () => {
   const neighbourhoods = ["Any Neighbourhood", "option1", "option2", "option3"];
 
   useEffect(() => {
-    fetchTableData('Parks').then(setParkMarkers);
+    fetchTableData("Parks").then(setParkMarkers);
+    fetchTableData("BikeRacksAndRepairStands").then(setBikeRackMarkers);
+    fetchTableData("Neighbourhoods").then(setNeighbourhoodMarkers);
+    fetchTableData("PropertyAssessments").then(setPropertyAssessmentMarkers);
+    fetchTableData("RainfallGauge").then(setRainfallGaugeMarkers);
+    fetchTableData("TrafficDisruptions").then(setTrafficDisruptionMarkers);
+    fetchTableData("VegetationAreasNaturalized").then(setVegetationAreaMarkers);
+    fetchTableData("busStopsByLandmark").then(setBusStopMarkers);
+    fetchTableData("trees").then(setTreeMarkers);
   }, []);
 
   return (
@@ -48,15 +91,17 @@ const MapView = () => {
       >
         <TileLayer
           url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          // url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+          //   url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
         />
-        {!!parkMarkers.length && parkMarkers.map((marker) =>
-          <Marker key={marker.id} position={[marker.LATITUDE, marker.LONGITUDE]}>
-            <Popup>
-              {marker.COMMON_NAME || 'An Unnamed Park'}
-            </Popup>
-          </Marker>
-        )}
+        {!!parkMarkers.length &&
+          parkMarkers.map((marker) => (
+            <Marker
+              key={marker.id}
+              position={[marker.LATITUDE, marker.LONGITUDE]}
+            >
+              <Popup>{marker.COMMON_NAME || "An Unnamed Park"}</Popup>
+            </Marker>
+          ))}
       </MapContainer>
     </>
   );
